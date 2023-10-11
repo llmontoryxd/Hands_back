a843c3d3354ac2f2f9fc9ed5938120a41b50eb04
llmontoryxd <montory2000@mail.ru>
2023-10-11 20:12:18 +0300

    Tried to fix memory leak

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 
 def get_phone_numbers(website: str, hard_search:bool=False, by_links:bool=True) -> list[str]:
     content = parse_website(website, hard_search)
+
     if by_links:
         phone_numbers = []
         soup = BeautifulSoup(content, 'html.parser')
@@ -28,6 +29,7 @@ def parse_website(website: str, hard_search: bool=False) -> str:
     if not hard_search:
         response = requests.get(website)
         content = response.text
+
     else:
         options = webdriver.FirefoxOptions()
         options.add_argument('--headless')
@@ -35,11 +37,12 @@ def parse_website(website: str, hard_search: bool=False) -> str:
         driver.get(website)
         reveal_hidden_number(driver, ['phone'])
         content = driver.page_source
+        driver.quit()
 
     return content
 
 
-def reveal_hidden_number(driver: webdriver.Firefox, key_words: list[str], timeout: float=1) -> None:
+def reveal_hidden_number(driver: webdriver.Firefox, key_words: list[str], timeout: float=0.5) -> None:
     time.sleep(timeout)
     for key_word in key_words:
         for element in driver.find_elements(By.XPATH, f'//button[contains(@class, "{key_word}")]'):
